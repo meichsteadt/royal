6043720208abbf8f84ec103564c8dcbc8bb12446
Matt Eichsteadt <matteichsteadt@gmail.com>
2017-10-13 20:02:00 -0700

    fix promo image

--- app/controllers/layouts_controller.rb
+++ app/controllers/layouts_controller.rb
@@ -3,6 +3,6 @@ class LayoutsController < ApplicationController
   # GET /layouts
   # GET /layouts.json
   def index
-    @slideshow = [Product.find(5), Product.find(8)]
+    @slideshow = ["https://www.homelegance.com/wp-content/uploads/2017/03/5400-1__1-750x500.jpg", "http://newclassicfurniture.com/wp-content/uploads/2016/06/00-2159_H1.jpg"]
   end
 end
