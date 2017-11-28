aa3d4bb01f9265d0691dc7cd27cb46a91d2221d7
Matt Eichsteadt <matteichsteadt@gmail.com>
2017-11-28 12:58:39 -0800

    change promo

--- app/controllers/layouts_controller.rb
+++ app/controllers/layouts_controller.rb
@@ -3,6 +3,6 @@ class LayoutsController < ApplicationController
   # GET /layouts
   # GET /layouts.json
   def index
-    @slideshow = ["https://www.homelegance.com/wp-content/uploads/2017/03/5400-1__1-750x500.jpg", "http://newclassicfurniture.com/wp-content/uploads/2016/06/00-2159_H1.jpg"]
+    @slideshow = [""]
   end
 end
